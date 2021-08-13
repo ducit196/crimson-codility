8c33bc9831d8a05f6c21dca91ccbc9ceb4d84d40
nbduc1996 <nbduc1996@gmail.com>
2021-08-13 18:10:31 +0700

    Test

--- src/nbduc/crimson/codility/arrays/OddOccurrencesInArray.java
+++ src/nbduc/crimson/codility/arrays/OddOccurrencesInArray.java
@@ -10,7 +10,6 @@ public class OddOccurrencesInArray {
     }
 
     public static int solution(int[] A) {
-        // write your code in Java SE 8
         if(null != A && A.length > 0) {
             int up = 0;
             for (int i = 0; i < A.length; i++) {
